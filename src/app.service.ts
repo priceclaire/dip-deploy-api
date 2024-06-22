3b310af752ab9432305e57f3d1b90d419a4f9083
Claire Price <claire.price@mail.com>
2024-06-22 23:05:15 +0100

    edit file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Attempt to deploy 1';
+    return 'Hello World! Attempt to deploy 2';
   }
 }
