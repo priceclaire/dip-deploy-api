ef84c59d097cf69d1d42da6ce0c050808c5fdf2a
Claire Price <claire.price@mail.com>
2024-07-08 23:28:16 +0100

    console log to troubleshoot

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,6 +5,8 @@ import { TodosModule } from './todos/todos.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from './datasource/typeorm.module';
 
+console.log(typeof process.env.DB_PASSWORD); // Should output 'string'
+
 @Module({
   imports: [TodosModule, ConfigModule.forRoot(), TypeOrmModule],
   controllers: [AppController],
