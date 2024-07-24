8a076bd700f3a7eafcba253ba574d7803a5dc7dc
Claire Price <claire.price@mail.com>
2024-07-24 20:58:09 +0100

    remove 's' from axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   });
 
   useEffect(() => {
-    axios.get(`https://${host}/api/todos`).then((response) => {
+    axios.get(`http://${host}/api/todos`).then((response) => {
       setTodos(response.data);
     });
   }, []);
@@ -40,7 +40,7 @@ function App() {
   };
 
   const addTodo = () => {
-    axios.post(`https://${host}/api/todos`, newTodo).then((response) => {
+    axios.post(`http://${host}/api/todos`, newTodo).then((response) => {
       setTodos(response.data);
     });
   };
