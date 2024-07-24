f15b6969ffb7936c7b9be368886c0d596b2f9a07
Claire Price <claire.price@mail.com>
2024-07-24 18:53:21 +0100

    add CorsOptions

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,26 @@
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, { cors: true });
+//   app.setGlobalPrefix('api');
+//   await app.listen(3000);
+// }
+// bootstrap();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'https://a609d02dff06146d48da289f58aef09b-1914571526.eu-north-1.elb.amazonaws.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
